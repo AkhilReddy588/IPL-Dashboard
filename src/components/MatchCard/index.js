c50537d31acb5e10a4bbc3c9f6321073990cb022
Nerati Akhil Reddy <akhil990836@gmail.com>
2024-05-08 23:51:20 +0530

    change

--- src/components/MatchCard/index.js
+++ src/components/MatchCard/index.js
@@ -10,7 +10,7 @@ const MatchCard = props => {
         src={competing_team_logo}
         alt={`competing team ${competing_team}`}
         className="recent-logo"
-      />
+      /> 
       <p className="recent-team">{competing_team}</p>
       <p className="result">{result}</p>
       <p className={statStyle}>{match_status}</p>
